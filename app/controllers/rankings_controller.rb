232fa0c0818cdab97acd202482105c5c6529c99d
yuhei.nagano <yuheinagano8942@gmail.com>
2018-10-13 05:29:08 +0000

    categorie

---
+++ app/controllers/rankings_controller.rb
@@ -0,0 +1,10 @@
+class RankingsController < ApplicationController
+  def popularityrecipe
+  end
+  
+  def eggrecipe
+  end
+  
+  def ricerecipe
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'toppages#index'
+  
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+  
+  get 'rankings/popularityrecipe', to: 'rankings#popularityrecipe'
+  get 'rankings/eggrecipe', to: 'rankings#eggrecipe'
+  get 'rankings/ricerecipe', to: 'rankings#ricerecipe'
+  
+  get 'signup', to: 'users#new'
+  resources :users, only: [:index, :show, :new, :create]
 end
